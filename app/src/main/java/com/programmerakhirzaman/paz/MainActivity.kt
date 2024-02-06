d14dea7076bdd484a62be437e718e019ae60a8ba
edohandoko67 <edohandoko67@gmail.com>
2024-02-06 16:28:11 +0700

    menu edit

--- app/src/main/java/com/programmerakhirzaman/paz/MainActivity.kt
+++ app/src/main/java/com/programmerakhirzaman/paz/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.programmerakhirzaman.paz
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.programmerakhirzaman.paz.activity.WebActivity
 import com.programmerakhirzaman.paz.databinding.ActivityMainBinding
+import java.net.URLEncoder
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val whatappUser = "+6289523526520"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -24,10 +27,28 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, WebActivity::class.java))
         }
 
+        binding.wa.setOnClickListener {
+            sendToWa()
+        }
+
+    }
+
+    private fun sendToWa() {
+        val message = "Hai kak, berikut pesan langsung dari Website PAZ. Saya mau konsultasi kak"
+        val url = "https://api.whatsapp.com/send?phone=$whatappUser&text=${URLEncoder.encode(message, "UTF-8")}"
+        try {
+            val packageManager = applicationContext.packageManager
+            packageManager.getPackageInfo("com.whatsapp", PackageManager.GET_ACTIVITIES)
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(url)
+            startActivity(intent)
+        } catch (e: PackageManager.NameNotFoundException){
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
+        }
     }
 
     private fun sendToCall() {
-        val telepon = "089523526520"
+        val telepon = "+6289523526520"
         val intent = Intent(Intent.ACTION_DIAL)
         intent.data = Uri.parse("tel:$telepon")
         startActivity(intent)
--- app/src/main/java/com/programmerakhirzaman/paz/activity/WebActivity.kt
+++ app/src/main/java/com/programmerakhirzaman/paz/activity/WebActivity.kt
@@ -2,10 +2,22 @@ package com.programmerakhirzaman.paz.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebViewClient
 import com.programmerakhirzaman.paz.R
+import com.programmerakhirzaman.paz.databinding.ActivityWebBinding
 
 class WebActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityWebBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityWebBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.webView.webViewClient = WebViewClient()
+        binding.webView.loadUrl("https://paz-official.000webhostapp.com")
+        val webSettings: WebSettings = binding.webView.settings
+        webSettings.javaScriptEnabled = true
     }
 }
