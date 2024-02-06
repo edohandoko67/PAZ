dc2beae790a703f026c4cc3730995501801bc0fe
edohandoko67 <edohandoko67@gmail.com>
2024-02-06 14:54:20 +0700

    hompage + webview

--- app/src/main/java/com/programmerakhirzaman/paz/MainActivity.kt
+++ app/src/main/java/com/programmerakhirzaman/paz/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.programmerakhirzaman.paz
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.programmerakhirzaman.paz.activity.WebActivity
+import com.programmerakhirzaman.paz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.call.setOnClickListener {
+            sendToCall()
+        }
+
+        binding.website.setOnClickListener {
+            startActivity(Intent(this, WebActivity::class.java))
+        }
+
+    }
+
+    private fun sendToCall() {
+        val telepon = "089523526520"
+        val intent = Intent(Intent.ACTION_DIAL)
+        intent.data = Uri.parse("tel:$telepon")
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/programmerakhirzaman/paz/activity/WebActivity.kt
@@ -0,0 +1,11 @@
+package com.programmerakhirzaman.paz.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.programmerakhirzaman.paz.R
+
+class WebActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
