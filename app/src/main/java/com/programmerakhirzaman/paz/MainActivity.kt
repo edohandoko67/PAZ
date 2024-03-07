1fb4d715031890cee5b42bfdde08be469d9aa58d
edohandoko67 <edohandoko67@gmail.com>
2024-03-07 15:38:14 +0700

    add multiple permission

--- app/src/main/java/com/programmerakhirzaman/paz/MainActivity.kt
+++ app/src/main/java/com/programmerakhirzaman/paz/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.programmerakhirzaman.paz
 
+import android.app.AlertDialog
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
@@ -9,7 +10,10 @@ import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
@@ -21,13 +25,26 @@ import java.net.URLEncoder
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val whatappUser = "+6289523526520"
-    private val INTERNET_PERMISSION_CODE = 100
-    private val REQUEST_CONTACTS_PERMISSION = 1
-    private val READ_EXTERNAL_STORAGE_PERMISSION_REQUEST_CODE = 102
-    private val MANAGE_EXTERNAL_STORAGE_PERMISSION_REQUEST_CODE = 103
-    private val FOREGROUND_SERVICE_PERMISSION_REQUEST_CODE = 104
-    private val WAKE_LOCK_PERMISSION_REQUEST_CODE = 105
-    private val ACCESS_FINE_LOCATION_PERMISSION_REQUEST_CODE = 106
+
+    private val requestPermissionsLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+            val isGranted = it.values.reduce { acc, next -> acc && next }
+            if (!isGranted) {
+                val builder = AlertDialog.Builder(this)
+                    .setTitle("Judul")
+                    .setMessage("Pesan")
+                    .setPositiveButton("Pengaturan") { dialog, _ ->
+                        dialog.dismiss()
+                        finish()
+                        val i = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                        i.data = Uri.parse("package:com.programmerakhirzaman.paz")
+                        startActivity(i)
+                    }
+                    .setCancelable(false)
+                val dialog = builder.create()
+                dialog.show()
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,25 +53,7 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        if (ContextCompat.checkSelfPermission(
-                this,
-                android.Manifest.permission.INTERNET
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            // You already have the permission
-            // You can proceed with your internet related operations
-        } else {
-            // You don't have the permission, request it
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(android.Manifest.permission.INTERNET),
-                INTERNET_PERMISSION_CODE
-            )
-        }
-
-        location()
-        storagePermission()
-        contactPermission()
+        checkPermissions()
 
         binding.call.setOnClickListener {
             sendToCall()
@@ -75,10 +74,15 @@ class MainActivity : AppCompatActivity() {
         fun showNotification(context: Context, title: String, message: String) {
             val channelId = "default_channel_id"
             val channelName = "Default Channel"
-            val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            val notificationManager =
+                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                val channel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT)
+                val channel = NotificationChannel(
+                    channelId,
+                    channelName,
+                    NotificationManager.IMPORTANCE_DEFAULT
+                )
                 notificationManager.createNotificationChannel(channel)
             }
 
@@ -94,14 +98,19 @@ class MainActivity : AppCompatActivity() {
 
     private fun sendToWa() {
         val message = "Hai kak, berikut pesan langsung dari Website PAZ. Saya mau konsultasi kak"
-        val url = "https://api.whatsapp.com/send?phone=$whatappUser&text=${URLEncoder.encode(message, "UTF-8")}"
+        val url = "https://api.whatsapp.com/send?phone=$whatappUser&text=${
+            URLEncoder.encode(
+                message,
+                "UTF-8"
+            )
+        }"
         try {
             val packageManager = applicationContext.packageManager
             packageManager.getPackageInfo("com.whatsapp", PackageManager.GET_ACTIVITIES)
             val intent = Intent(Intent.ACTION_VIEW)
             intent.data = Uri.parse(url)
             startActivity(intent)
-        } catch (e: PackageManager.NameNotFoundException){
+        } catch (e: PackageManager.NameNotFoundException) {
             startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
         }
     }
@@ -113,90 +122,34 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
-    private fun contactPermission() {
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_CONTACTS)
-            != PackageManager.PERMISSION_GRANTED
-        ) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf(android.Manifest.permission.READ_CONTACTS),
-                    REQUEST_CONTACTS_PERMISSION
-                )
-            }
-        } else {
-            // Permission has already been granted
-            // You can proceed with your contact related tasks
+    private fun checkPermissions() {
+        val checkPermissionsResult = requiredPermissions.all {
+            ContextCompat.checkSelfPermission(
+                applicationContext,
+                it
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+
+        if (!checkPermissionsResult) {
+            requestPermissionsLauncher.launch(requiredPermissions)
         }
     }
 
-    private fun location() {
-        if (ContextCompat.checkSelfPermission(
-                this,
-                android.Manifest.permission.FOREGROUND_SERVICE
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(android.Manifest.permission.FOREGROUND_SERVICE),
-                FOREGROUND_SERVICE_PERMISSION_REQUEST_CODE
-            )
-        }
-
-        if (ContextCompat.checkSelfPermission(
-                this,
-                android.Manifest.permission.WAKE_LOCK
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(android.Manifest.permission.WAKE_LOCK),
-                WAKE_LOCK_PERMISSION_REQUEST_CODE
-            )
-        }
-
-        if (ContextCompat.checkSelfPermission(
-                this,
-                android.Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
-                    ACCESS_FINE_LOCATION_PERMISSION_REQUEST_CODE
-                )
-            }
-        }
+    private val requiredPermissions = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        arrayOf(
+            android.Manifest.permission.ACCESS_FINE_LOCATION,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION,
+            android.Manifest.permission.READ_CONTACTS,
+            android.Manifest.permission.READ_MEDIA_IMAGES
+        )
+    } else {
+        arrayOf(
+            android.Manifest.permission.ACCESS_FINE_LOCATION,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION,
+            android.Manifest.permission.READ_CONTACTS,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            android.Manifest.permission.READ_EXTERNAL_STORAGE
+        )
     }
-
-    private fun storagePermission() {
-        if (ContextCompat.checkSelfPermission(
-                this,
-                android.Manifest.permission.READ_EXTERNAL_STORAGE
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-                ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
-                    READ_EXTERNAL_STORAGE_PERMISSION_REQUEST_CODE
-                )
-            }
-        }
-
-        if (ContextCompat.checkSelfPermission(
-                this,
-                android.Manifest.permission.MANAGE_EXTERNAL_STORAGE
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(android.Manifest.permission.MANAGE_EXTERNAL_STORAGE),
-                MANAGE_EXTERNAL_STORAGE_PERMISSION_REQUEST_CODE
-            )
-        }
-    }
-
 
 }
